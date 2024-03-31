502921db5e29a849e279de3fc7f13d85527a882d
unknown <andiharunlrasyid14@gmail.com>
2024-04-01 06:43:02 +0800

    fist commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAlert() {
+  alert(Date(), tampil(), show());
+  Date();
+
+  let tanggal = new date();
+  documentge.getElementByid("hasil").innerHTML = tanggal;
+
+  function tampil() {
+    alert(
+      "Pemenang liga1 tahun lalu nih boss tabrak dongg kaka"
+    );
+  }
+
+  function show() {
+    alert("klub kebanggaan sulawesi nih bosss ");
+  }
+}
